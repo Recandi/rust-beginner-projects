030945f46d176e0e538a489e32356b94c67ae1ea
p.classen <p.classen@otterbach.de>
2019-10-14 15:53:15 +0200

    get tablesize as input + a bit of documenting

--- src/multiplication_table.rs
+++ src/multiplication_table.rs
@@ -1,14 +1,32 @@
+use std::io;
+use io::Write;
 ///Prints nicely formatted multiplication table to the console
 pub fn multiply(){
-    print!(" ");
-    for i in 0..10{
-        print!("{:4}", i);
+
+    print!("Please enter size of table: ");
+    
+    //Get tablesize as input and parse to number + 1
+    io::stdout().flush().unwrap();
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Reading from CL went Wrong");
+    input = input.trim().to_string();
+    let mut x : u32= input.parse().expect("NaN");
+    x = x + 1;
+
+    //find biggest number, cast to string to get necessary spacing for formatting
+    let space = (x * x).to_string().len() + 1;
+    
+    //vertical index offset
+    print!("{:3}", "");
+    for i in 0..x{
+        print!("{:s$}", i, s=space);
     }
+    
     println!("");
-    for i in 0..10{
-        print!("{}", i);
-        for e in 0..10{
-            print!{"{:4}", i * e};
+    for i in 0..x{
+        print!("{:3}", i);
+        for e in 0..x{
+            print!{"{:s$}", i * e, s=space};
         }
         println!("");
     }
